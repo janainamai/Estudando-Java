533e1fa6c2003d21e3d0253dd62a2e8289ff7bdc
Janaina Mai <janainamai@hotmail.com>
2021-05-10 14:00:56 -0300

    att

---
+++ src/main/java/br/com/janaina/devdojo/Jdatas/KTemporalAdjuster.java
@@ -0,0 +1,41 @@
+package br.com.janaina.devdojo.Jdatas;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+
+class ObterProximoDiaUtil implements TemporalAdjuster {
+
+	@Override
+	public Temporal adjustInto(Temporal temporal) {
+		DayOfWeek dayOfWeek = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+		int addDays;
+		switch (dayOfWeek) {
+		case FRIDAY:
+			addDays = 3;
+			break;
+		case SATURDAY:
+			addDays = 2;
+			break;
+		default:
+			addDays = 1;
+		}
+		return temporal.plus(addDays, ChronoUnit.DAYS);
+	}
+
+}
+
+public class KTemporalAdjuster {
+	public static void main(String[] args) {
+		// data de hoje normal
+		LocalDate now = LocalDate.now();
+		System.out.println(now + " " + now.getDayOfWeek());
+		
+		// aa
+		
+
+	}
+}
