0c9d665dbcdcec3e3b2a9a9e9f5980e2932b54a8
Janaina Mai <janainamai@hotmail.com>
2021-05-12 13:09:35 -0300

    Estudando ResourceBundle

---
+++ src/main/java/br/com/janaina/devdojo/Lresourcebundle/Bundle.java
@@ -0,0 +1,25 @@
+package br.com.janaina.devdojo.Lresourcebundle;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Bundle {
+	public static void main(String[] args) {
+		System.out.println(Locale.getDefault());
+		ResourceBundle bundle = ResourceBundle.getBundle("messages", new Locale("pt", "BR"));
+		
+		//verificando se existe uma chave
+		boolean chave = bundle.containsKey("chave");
+		
+		//buscando um valor atraves de uma chave
+		bundle.getString("good.morning");
+		//saída: Good Morning
+		
+		//imagine que o usuário agora trocou o local de acesso
+		bundle = ResourceBundle.getBundle("messages", new Locale("en", "US"));
+		
+		//buscando o mesmo valor de antes
+		bundle.getString("good.morning");
+		//saída: Bom dia!
+	}
+}
