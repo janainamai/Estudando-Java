9292cdc6ad38ba023f53028a1ae418912acb6283
Janaina Mai <janaina.mai@senior.com.br>
2021-06-30 10:47:01 -0300

    Estudando Directory Stream

---
+++ src/main/java/br/com/janaina/devdojo/Onio/ISimpleFileVisitor.java
@@ -0,0 +1,10 @@
+package br.com.janaina.devdojo.Onio;
+
+public class ISimpleFileVisitor {
+	public static void main(String[] args) {
+		// com SimpleFileVisitor, podemos nevegar dentro das pastas do nosso diretório
+		// anteriormente com DirectoryStream não poderiamos fazer isso
+		
+		
+	}
+}
